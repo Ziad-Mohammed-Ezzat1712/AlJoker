30c9bbd767043ae7154c49264f438fc72d83df26
Ziad-Mohammed-Ezzat1712 <ziadmohammed1712@gmail.com>
2025-08-31 14:59:06 +0300

    edit31/8

--- src/Components/MostSearchedCars/MostSearchedCars.jsx
+++ src/Components/MostSearchedCars/MostSearchedCars.jsx
@@ -72,7 +72,7 @@ export default function MostSearchedCars() {
   const swiperRef = useRef(null);
 
   return (
-    <div className="py-12 xl:px-0 px-10 bg-white">
+    <div className="py-12 xl:px-0  bg-white">
       {/* العنوان */}
       <h2 className="text-[40px] md:text-[40px] font-semibold text-center text-[#050B20] mb-8">
         The Most Searched Cars
